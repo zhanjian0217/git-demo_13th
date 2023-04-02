d652ff021a0064f408effe3c7c5068810535178a
Zhan <gian910147@gmail.com>
2023-04-02 19:39:12 +0800

    add: cat.rb

---
+++ cat.rb
@@ -0,0 +1,18 @@
+# frozend_string_literal: true
+
+class Cat
+  attr_reader :name, :age
+
+  def initialize(name: nil, age: nil)
+    @name = name
+    @age = age
+  end
+
+  def run
+    puts '跑跑跑'
+  end
+
+  def after_one_year
+    @age += 1
+  end
+end
